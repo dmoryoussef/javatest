aa55337f24e7f68b3293a6a69511582c27604d14
dmoryoussef <87531619+dmoryoussef@users.noreply.github.com>
2022-06-22 09:10:13 -0500

    Add files via upload

---
+++ JavaApplication19.java
@@ -0,0 +1,16 @@
+package javaapplication19;
+
+/**
+ *
+ * @author dmory
+ */
+public class JavaApplication19 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
